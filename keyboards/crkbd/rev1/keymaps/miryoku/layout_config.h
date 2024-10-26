3617577e990c03f0fa00db99ff0cfed402ddc955
Harrison Tyson <hty13@uclive.ac.nz>
2024-10-27 01:03:56 +1300

    replacee screenshot key with a repeat key

--- keyboards/crkbd/rev1/keymaps/miryoku/layout_config.h
+++ keyboards/crkbd/rev1/keymaps/miryoku/layout_config.h
@@ -9,7 +9,7 @@
 ) \
 LAYOUT_split_3x6_3( \
 DM_REC1,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  DM_PLY1, \
-CW_TOGG,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  KC_PSCR, \
+CW_TOGG,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  QK_REP, \
 QK_LOCK,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  DF(U_BASE) , \
                       K32,  K33,  K34,         K35,  K36,  K37 \
 )
